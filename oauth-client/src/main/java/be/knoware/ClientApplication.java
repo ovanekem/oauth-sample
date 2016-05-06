b7d6816f2afcc12112bb83aaa9b1b645d2c2f065
Olivier Vanekem <olivier.vanekem@knoware.be>
2016-05-06 23:21:21 +0200

    client app

--- oauth-client/src/main/java/be/knoware/ClientApplication.java
+++ oauth-client/src/main/java/be/knoware/ClientApplication.java
@@ -1,6 +1,7 @@
 package be.knoware;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  * Created by ovanekem on 03/05/16.
  */
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ClientApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
